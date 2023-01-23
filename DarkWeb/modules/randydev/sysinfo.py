99e298707ee4d774a22f0f1f6374e206997f59f1
xtsea <90479255+xtsea@users.noreply.github.com>
2023-01-23 16:48:03 +0700

    do manual `.sh apt install neofetch -y`

--- DarkWeb/modules/randydev/sysinfo.py
+++ DarkWeb/modules/randydev/sysinfo.py
@@ -16,19 +16,13 @@ async def neofetch(client: Client, message: Message):
     chat_id = message.chat.id
     noob = await message.reply_text("`Prossing.....`")
     try:
-        await asyncio.sleep(2)
-        await noob.edit("`wait for Installing neofetch`")
-        await shell_exec("sudo apt-get install neofetch -y")[0]
-    except Exception:
-        return ""
-    try:
         neofetch = (await shell_exec("neofetch --stdout"))[0]
-        await noob.edit("`Uloading....`")
         carbon = await make_carbon(neofetch)
+        await noob.edit("`Uloading....`")
         await client.send_photo(chat_id, carbon, caption=f"Carbonised by {client.me.mention}")
         await noob.delete()
     except Exception:
-        return ""
+        pass 
 
 # you can add modules
 
