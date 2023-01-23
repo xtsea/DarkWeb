196af6a30cc6e5df2e30781322ad5e1e0b37bdd6
xtsea <90479255+xtsea@users.noreply.github.com>
2023-01-23 16:01:12 +0700

    Update sysinfo.py

--- DarkWeb/modules/randydev/sysinfo.py
+++ DarkWeb/modules/randydev/sysinfo.py
@@ -15,12 +15,18 @@ from pykillerx import *
 async def neofetch(client: Client, message: Message):
     noob = await message.reply_text("`Prossing.....`")
     try:
+        error_install = (await shell_exec("neofetch"))[0]
+        if not error_install:
+           return await noob.edit("**try** `.bash sudo apt-get install neofetch -y`")
+    except Exception:
+        return ""
+    try:
         neofetch = (await shell_exec("neofetch --stdout"))[0]
         carbon = await make_carbon(neofetch)
         await client.send_photo(message.chat.id, carbon)
         await noob.delete()
-    except BaseException:
-        pass
+    except BaseException as e:
+        return await noob.edit(f"**ERROR** `{e}`")
 
 # you can add modules
 
