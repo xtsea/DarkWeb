3143d3e95a552fc3a97104a8818c9b0356a58d47
xtsea <90479255+xtsea@users.noreply.github.com>
2023-01-26 19:50:47 +0700

    Update chatbot.py

--- DarkWeb/modules/randydev/chatbot.py
+++ DarkWeb/modules/randydev/chatbot.py
@@ -9,15 +9,16 @@ from pyrogram import *
 from pyrogram.types import *
 from pyrogram import Client as ren 
 from pyrogram.errors import MessageNotModified
+
 from DarkWeb.helper.cmd import *
 from DarkWeb.helper.what import *
 
-from pykillerx.helper import *
 from pykillerx.help import *
-from pykillerx.helper.tools import *
 from pykillerx import *
 
-from config import OPENAI_API
+from config import *
+
+OPENAI_API_KEY = OPENAI_API
 
 @ren.on_message(filters.command("cask", cmd) & filters.user(901878554) & ~filters.me)
 @ren.on_message(filters.command("ask", cmd) & filters.me)
@@ -27,7 +28,7 @@ async def openai(c, m):
     question = m.text.split(" ", maxsplit=1)[1]
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {OPENAI_API}",
+        "Authorization": f"Bearer {OPENAI_API_KEY}",
     }
 
     json_data = {
