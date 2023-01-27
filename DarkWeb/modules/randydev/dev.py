dda64cb14a6b57084c99466aebee315df005f1d2
xtsea <90479255+xtsea@users.noreply.github.com>
2023-01-28 01:34:57 +0700

    fix

--- DarkWeb/modules/randydev/dev.py
+++ DarkWeb/modules/randydev/dev.py
@@ -83,8 +83,13 @@ async def evaluation_cmd_t(client, message):
     else:
         evaluation = "Success"
 
-    final_output = f"**EVAL**:\n<pre language="python">{cmd}</pre>\n\n**OUTPUT**:\n<pre language="python">{evaluation.strip()}</pre>\n"
+    final_output = f"""
+    **EVAL**:
+    <pre language="python">{cmd}</pre>
 
+    **OUTPUT**:
+    <pre language="python">{evaluation.strip()}</pre>
+    """
     if len(final_output) > 4096:
         with open("eval.txt", "w+", encoding="utf8") as out_file:
             out_file.write(final_output)
