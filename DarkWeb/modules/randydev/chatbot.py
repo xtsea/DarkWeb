78818f52d9499f8d2ffed8c43feb1c112369aecf
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-11 05:33:08 +0700

    Update chatbot.py

--- DarkWeb/modules/randydev/chatbot.py
+++ DarkWeb/modules/randydev/chatbot.py
@@ -42,9 +42,41 @@ async def openai(c, m):
     except Exception:
         await msg.edit("Yahh, sorry i can't get your answer.")
 
+# Credits by @xtsea
+
+@ren.on_message(filters.command("gpti", cmd) & filters.me)
+async def openai_image(c, m):
+    if len(m.command) == 1:
+        return await m.reply(f"use command <code>.{m.command[0]} [question]</code> to generate a random image using the API.")
+    question = m.text.split(" ", maxsplit=1)[1]
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPENAI_API}",
+    }
+
+    prompt = f"generate a random image {question}"
+    
+    json_data = {
+        "engine": "image-alpha-001",
+        "prompt": prompt,
+        "stop": None,
+        "max_tokens": 0,
+        "temperature": 0.5,
+    }
+    msg = await m.reply("Wait a moment looking for your answer..")
+    try:
+        response = (await http.post("https://api.openai.com/v1/completions", headers=headers, json=json_data)).json()
+        image_url = response["choices"][0]["text"].strip()
+        await c.send_photo(m.chat.id, photo=image_url)
+        await msg.delete()
+    except Exception:
+        await msg.edit("Yahh, sorry i can't get your answer.")
+
+
 add_command_help(
     "chatbot",
     [
         [f"ask [question]", "to ask questions using the API."],
+        [f"gpti [image]", "to random image using the API."],
     ],
 )
