d11a13a2368eb120893342c75a32f7f1dd882c75
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-11 19:11:20 +0700

    _

--- DarkWeb/modules/randydev/tools.py
+++ DarkWeb/modules/randydev/tools.py
@@ -10,14 +10,16 @@ from DarkWeb.modules.randydev.dev import shell_exec
 from pykillerx.help import *
 
 @ren.on_message(filters.command("convert", cmd) & filters.me)
-async def photo_as_sticker(c, m):
+async def photo_as_sticker(c, m: Message):
     try:
+        pro = await m.reply_text("`Prossing....`")
         goblok_lu = m.text.split(" ", 1)[1]
         ran = m.reply_to_message
         if not ran and not goblok_lu: 
-           return await m.reply("**Please Reply**")
+           return await pro.edit("**Please Reply [image/background]**")
 
         if goblok_lu.startswith("-p"):
+           await pro.delete()
            file_name = "downloads/ran.webp"
            upload = await ran.download()
            (await shell_exec("cd downloads && cp *.jpg ran.webp"))[0]
@@ -26,6 +28,7 @@ async def photo_as_sticker(c, m):
            os.remove(upload)
 
         if goblok_lu.startswith("-r"):
+           await pro.delete()
            file_name = "downloads/ran.webp"
            upload = await ran.download()
            (await shell_exec("cd downloads && cp *.png ran.webp"))[0]
