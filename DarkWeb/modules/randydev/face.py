8774c71d110ae63632a7d3b6a39d52c1d28dd921
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-12 02:11:29 +0700

    _

--- DarkWeb/modules/randydev/face.py
+++ DarkWeb/modules/randydev/face.py
@@ -20,7 +20,7 @@ async def face_detect(c: Client, m: Message):
     pro = await m.reply_text("`Whacking face detect.......`")
     await asyncio.sleep(5)
     if not m.reply_to_message or not m.reply_to_message.photo:
-        await m.reply("Please reply to a photo to detect faces.")
+        await pro.edit("Please reply to a photo to detect faces.")
         return
 
     file_id = m.reply_to_message.photo.file_id
