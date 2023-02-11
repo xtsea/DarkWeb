2a5fb367b4e6f56fbafc83e61303d513e1fa0845
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-12 02:53:08 +0700

    new add image cartoon

--- DarkWeb/modules/randydev/face.py
+++ DarkWeb/modules/randydev/face.py
@@ -4,6 +4,8 @@ import os
 import asyncio
 import cv2
 import numpy as np
+import requests
+from io import BytesIO
 from pyrogram import Client as ren
 from pyrogram.types import *
 from pyrogram import *
@@ -41,3 +43,26 @@ async def face_detect(c: Client, m: Message):
         os.remove(cleared)
     except BaseException:
         pass
+
+@ren.on_message(filters.command(["toonify", "cartoon"], cmd) & filters.me)
+async def toonify_handler(client, message):
+    url = "https://api.deepai.org/api/toonify"
+    if not m.reply_to_message or not m.reply_to_message.photo:
+        await m.reply_text("Please reply to a photo to convert to cartoon or comic style.")
+        return
+    
+    file_id = m.reply_to_message.photo.file_id
+    photo_bytes = await c.download_media(file_id)
+   
+    response = requests.post(url, files={"image": photo_bytes}, headers={"api-key": "4871e0ba-3bb6-40d8-b600-f415877c7606") # DON'T THIS STEAL 
+    result = response.json()
+    if result["success"]:
+        cartoon_bytes = BytesIO(requests.get(result["output_url"]).content)
+        cartoon_img = np.asarray(bytearray(cartoon_bytes.read()), dtype=np.uint8)
+        cartoon_img = cv2.imdecode(cartoon_img, cv2.IMREAD_COLOR)
+        cartoon_file = "cartoon.jpg"
+        cv2.imwrite(cartoon_file, cartoon_img)
+        
+        await m.reply_photo(cartoon_file)
+    else:
+        await m.reply_text("Failed to convert the photo to cartoon or comic style.")
