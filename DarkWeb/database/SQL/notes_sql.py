a25091b85dc10a2ab79acf8856c223c5ddd42ee6
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-12 12:06:03 +0700

    Create notes_sql.py

---
+++ DarkWeb/database/SQL/notes_sql.py
@@ -0,0 +1,46 @@
+try:
+    from DarkWeb.database.SQL import BASE, SESSION
+except ImportError:
+    raise AttributeError
+
+from sqlalchemy import Column, String
+
+class Note(BASE):
+    __tablename__ = "notes"
+    id = Column(Integer, primary_key=True)
+    chat_id = Column(String(14))
+    keyword = Column(String(128))
+    text = Column(Text)
+
+    def __init__(self, chat_id, keyword, text):
+        self.chat_id = chat_id
+        self.keyword = keyword
+        self.text = text
+
+def add_note(chat_id, keyword, text):
+    new_note = Note(chat_id, keyword, text)
+    SESSION.add(new_note)
+    SESSION.commit()
+    SESSION.close()
+
+def get_note_text(chat_id, keyword):
+    try:
+        note = SESSION.query(Note).filter_by(chat_id=chat_id, keyword=keyword).first()
+        if note:
+            return note.text
+        else:
+            return None
+    finally:
+        SESSION.close()
+
+def delete_note(chat_id, keyword):
+    try:
+        note = SESSION.query(Note).filter_by(chat_id=chat_id, keyword=keyword).first()
+        if note:
+            SESSION.delete(note)
+            SESSION.commit()
+            return True
+        else:
+            return False
+    finally:
+        SESSION.close()
