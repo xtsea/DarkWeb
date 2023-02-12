6a018acef00ac8448e3977312de88e014bba44f4
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-12 15:26:45 +0700

    _

--- DarkWeb/modules/randydev/notes.py
+++ DarkWeb/modules/randydev/notes.py
@@ -1,19 +1,5 @@
-"""
-Project [DarkWeb](https://github.com/TeamKillerX/DarkWeb) is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
-""""
-
 # Copyright (C) 2020-2022 TeamDerUntergang <https://github.com/TeamDerUntergang>
 
-from pyrogram import Client as ren 
 from pyrogram.types import * 
 from DarkWeb.helper.cmd import *
 from pykillerx import *
@@ -21,7 +7,8 @@ from pykillerx.helper import *
 from pykillerx.help import *
 from DarkWeb import *
 from config import *
-from utils.core import (
+
+from DarkWeb.utils.core import (
     edit,
     extract_args,
     forward,
@@ -30,7 +17,7 @@ from utils.core import (
     reply_msg,
     randydev,
     send_log,
-)
+) 
 
 
 def notes_init():
