570c8f5d2226e7928f86cc9bf30f4fed4794a26f
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-13 00:37:48 +0700

    _

--- config.py
+++ config.py
@@ -34,7 +34,6 @@ LOG_VERBOSE = sb(environ.get('LOG_VERBOSE', 'False'))
 
 RMBG_API = getenv("RMBG_API")
 OPENAI_API = getenv("OPENAI_API")
-REN_LANG = environ.get("REN_LANG", "en")
 # don't kanger repo this !!!
 CHANNEL = "@RendyProjects"
 SUPPORT = "@pantekyks"
