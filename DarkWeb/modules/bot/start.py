2435c9463fff060d5d6fc12faed5b4dfb089f37f
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-17 15:20:01 +0700

    Update start.py

--- DarkWeb/modules/bot/start.py
+++ DarkWeb/modules/bot/start.py
@@ -9,7 +9,7 @@ from pyrogram.types import *
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(client: Client, message: Message):
-    welcome = f"""
+    welcome = 
 Hey there! {message.from_user.mention}
 I'm an <b>administration</b> bot that helps you to keep your groups safe from <b>spammers</b>
 If you want to use me for your groups, note that I'm more useful on a pyrogram of groups and you also need to <b>setup a new bot.</b>
