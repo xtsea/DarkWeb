46bb066660a8c46c47ffca4e64ee942e847efa6f
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-17 15:20:22 +0700

    _

--- DarkWeb/modules/bot/start.py
+++ DarkWeb/modules/bot/start.py
@@ -14,7 +14,7 @@ Hey there! {message.from_user.mention}
 I'm an <b>administration</b> bot that helps you to keep your groups safe from <b>spammers</b>
 If you want to use me for your groups, note that I'm more useful on a pyrogram of groups and you also need to <b>setup a new bot.</b>
 So if you don't wish to self-host, @MissRose_bot might be a better choice for
-"""
+
     try:
         await message.reply_text(
             text=welcome,
