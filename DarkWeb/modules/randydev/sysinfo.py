ce6293c0ad5566edeaf8aad09f786e433378e65a
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-23 02:07:25 +0700

    fixed

--- DarkWeb/modules/randydev/sysinfo.py
+++ DarkWeb/modules/randydev/sysinfo.py
@@ -32,7 +32,7 @@ async def sysinfo(c: Client, m: Message):
     chat_id = m.chat.id
     pro = await m.reply_text("`Prossing....`")
     try:
-        sysinfo = (await shell_exec("neofetch | sed 's/\x1B\\[[0-9;\\?]*[a-zA-Z]//g'")
+        sysinfo = (await shell_exec("neofetch | sed 's/\x1B\\[[0-9;\\?]*[a-zA-Z]//g'"))[0]
         carbon = await make_carbon(sysinfo)
         await c.send_photo(chat_id, carbon, caption=f"Carbonised by {c.me.mention}")
         await pro.delete()
