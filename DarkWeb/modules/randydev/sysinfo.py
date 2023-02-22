0469c451faaad5f76021ece7abb271002d2ea2e0
xtsea <90479255+xtsea@users.noreply.github.com>
2023-02-23 01:58:33 +0700

    Update sysinfo.py

--- DarkWeb/modules/randydev/sysinfo.py
+++ DarkWeb/modules/randydev/sysinfo.py
@@ -24,6 +24,17 @@ async def neofetch(client: Client, message: Message):
     except Exception:
         pass 
 
-# you can add modules
+# Copyright: https://t.me/xtsea
+# your kanger this : please don't remove copyright 
 
-# this code
+@ren.on_message(filters.command("sysinfo", cmd) & filters.me)
+async def sysinfo(c: Client, m: Message):
+    chat_id = m.chat.id
+    pro = await m.reply_text("`Prossing....`")
+    try:
+        sysinfo = (await shell_exec("neofetch | sed 's/\x1B\\[[0-9;\\?]*[a-zA-Z]//g'")
+        carbon = await make_carbon(sysinfo)
+        await c.send_photo(chat_id, carbon, caption=f"Carbonised by {c.me.mention}")
+        await pro.delete()
+    except Exception:
+        pass
